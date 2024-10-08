539f0f0e9c3be9796b09f1c928026f7b9b32faad
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-08 14:16:26 -0500

    name

---
+++ main.py
@@ -0,0 +1,15 @@
+# thinger for url
+# ?vscode-coi=
+
+# game info like the version!!!!!
+version = 0.1
+
+# Print out da game version :D
+print("Python Insider v"+str(version))
+
+# whats ur name
+print("Input your name:")
+name = input('> ')
+print("Welcome. "+name+'.')
+
+# The loop will go here
